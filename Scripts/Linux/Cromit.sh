b03413b518ed927293edda138b3f8a36ff80be1e
AmatheræWorld <64475927+AmatheraeWorld@users.noreply.github.com>
2020-06-08 23:51:31 +0200

    Add files via upload

--- Scripts/Linux/Cromit.sh
+++ Scripts/Linux/Cromit.sh
@@ -1,22 +1,73 @@
 #!/bin/bash
-
+# version 1.6 fecha 8/6/2020
+greenColour="\e[0;32m\033[1m"
+endColour="\033[0m\e[0m"
+redColour="\e[0;31m\033[1m"
+blueColour="\e[0;34m\033[1m"
+yellowColour="\e[0;33m\033[1m"
+purpleColour="\e[0;35m\033[1m"
+turquoiseColour="\e[0;36m\033[1m"
+grayColour="\e[0;37m\033[1m"
+echo 
+echo -e "                            .,,,,..                                  " 
+echo -e "                      ..,,//*,,..... ......,.                        " 
+echo -e "                ,,*,,....(/*/,,,....../,....,,                       " 
+echo -e "              ***//*,*...*%##(/***,,,,/,*..../.                      " 
+echo -e "             /(*//(/*/(*///(###(///**,(,#******.                     " 
+echo -e "             #((((((//##((#%&%&%%#(/%#&&##**(/*.                     " 
+echo -e "             %##%%(#((/%&&&&@%#(###/#(/////#(*                       " 
+echo -e "              %%##(%%%###(***,,,//,./,,,*//.                         " 
+echo -e "                .#%##%%##((/**,*(##(*,,***.                          " 
+echo -e "                     /#####((/(#%%&%#*,*.                            " 
+echo -e "                           ##(((/(/,,*,                              " 
+echo -e "                      *////########(///**#/****/(*                   " 
+echo -e "                  *#%*#&&&&(//((#%&&(#,#((/**/*,%&*,/(,.             " 
+echo -e "                 (&/,*&%(###////*/*###(#%////*/%/,,.(#/              " 
+echo -e "                /##(*/(*,**#/*//*,,,((####%&&((((((&(**/,            " 
+echo -e "               .%#%#/,//.,,/*/(//,.,/#%#((###(((&%##&.               " 
+echo -e "                .%%%######/%#%%##(##(((((###%%%%&#,,,,.              " 
+echo -e "                 ##%%%#%%(###(%%%%%%###/(((,   //**,...,             " 
+echo -e "              ./*((##%%%#(##%%%####%#**/#(.     ///**,.  ..          " 
+echo -e "           &&,..*/##%%%&#*((#((#(((//*/##*       ////%&%#,#((        " 
+echo -e "           /%%*,,(#%&%###(/*,,*/#%###/,**         /&#(%#/*//(#.      " 
+echo -e "        ,/**/(*@&&&&@#&%%##(%&%/(((((*%&(&.       /#/**,,,...,       " 
+echo -e "       ,*,(####, %&%&&##%&&#&&%#&&&&#&&%((#       .****//*,,,*,,     " 
+echo -e "      ./#,//(.  .&#(/(////((&&%///(*.. . .,       ,/,      **,,,..   " 
+echo -e "      .*//../.  &&/**/////((&&&/*,..      .        (*.        .*(.   " 
+echo -e "               ,&/*,*///*/((&&&/,,..      .,                    ,    "
+echo -e "              .&(**,*/****/(&&&/,...       ,                         "
+echo -e "             .#&/**,****,*/(&&&/. .        ..                        "
+echo -e "             .&//**,,**,,,/(&&&/  .         .                        "
+echo -e "             .///**,,**,,,/(&&&*  .           .                      "
+echo -e "            .((/*,,,,,,,,*/(&@&*  .      .     .                     "
+echo -e "            /@&#(,,,,,,,,*/(%@&*.           ..  .                    "
+echo -e "           .&%*(&&%%#*,,.,//&&%/,.. .*,,.*///*#/..                   "
+echo -e "            /%%%%(#&&&&&&@@/%@(*&&&&%%###/**.../.                    " 
+echo -e "                     ../(##%/(@(&%(#/&%%%%%%%%%/.                    " 
+echo -e "                            **    #&&@&&&&&&&&&(,                    " 
+echo -e "                                        ....                         "
+echo -e "                                                                     "
+echo -e "${purpleColour}==================(${endColour}${greenColour} Cromit v1.6 8/6/2020 ${endColour}${purpleColour})==========================${endColour}"
+echo 
 echo -e " Escoge opcion\n\n "
-echo "[1] crontab -l --> para ver las tareas programadas "
-echo "[2] systemctl list-timers --> para ver tareas con temporizadores y cuanto les queda "
-echo "[3] Procmon "
-echo "[4] Procmon v2 "
-echo "[5] Cronmonit "
-echo "[6] tail -f [archivo] --> nos muestra en tiempo real el archivo, por si cambia con un trabajo cron "
-echo "[7] watch -n 1 ls -l [Comando] -->  para ver cada segundo como cambia un comando que se le pase "
-echo "[8] watch -n 1 du -hc [archivo] --> para ver como cambia el peso de un archivo cada segundo "
-echo "[9] ltrace [programa] --> para ver las llamadas que hace un programa, se usa en reversing "
-echo "[10] find \-name *config* --> Busca en el directorio en el que se esta todos los archivos de configuracion "
-echo "[11] find \-user [usuario] 2>/dev/null --> ver recursos de los que el usuario es propietario y elimina los errores "
-echo "[12] find \-perm -u=s 2>/dev/null --> Busca archivos SUID que se ejecutan con permisos de propietario " 
-echo "[13] Puertos internos abiertos proc/net/tcp "
-echo "[14] Grupos en los que estamos "ID" "
-echo "[15] Tipo de archivo file" archivo" "
-echo -e "[16] w --> para ver que usuarios estan conectados ahora mismo al sistema  \n"
+echo -e "${purpleColour}[1]${endColour}${greenColour} crontab -l${endColour} --> Para ver las tareas programadas "
+echo -e "${purpleColour}[2]${endColour}${greenColour} systemctl list-timers${endColour} --> Para ver tareas con temporizadores y cuanto les queda "
+echo -e "${purpleColour}[3]${endColour}${greenColour} Procmon${endColour} "
+echo -e "${purpleColour}[4]${endColour}${greenColour} Procmon v2${endColour} "
+echo -e "${purpleColour}[5]${endColour}${greenColour} Cronmonit${endColour} "
+echo -e "${purpleColour}[6]${endColour}${greenColour} tail -f${endColour} ${blueColour}[archivo]${endColour} --> Nos muestra en tiempo real el archivo, por si cambia con un trabajo cron "
+echo -e "${purpleColour}[7]${endColour}${greenColour} watch -n 1${endColour} ${blueColour}[comando]${endColour} -->  Para ver cada segundo como cambia un comando que se le pase ${blueColour}[ls -laR]${endColour} para ver si un cron crea o modifica un archivo "
+echo -e "${purpleColour}[8]${endColour}${greenColour} watch -n 1 du -hc${endColour} ${blueColour}[archivo]${endColour} --> Para ver como cambia el peso de un archivo cada segundo por si un cron escribe en un archivo"
+echo -e "${purpleColour}[9]${endColour}${greenColour} ltrace${endColour}${blueColour} [programa]${endColour} --> Para ver las llamadas que hace un programa, se usa en reversing ${redColour}[requiere ltrace instalado en la maquina]${endColour}"
+echo -e "${purpleColour}[10]${endColour}${greenColour} tree${endColour} --> Para ver en formato arbol si una tarea cron crea o elimina un archivo ${redColour}[requiere tree instalado en la maquina]${endColour}"
+echo -e "${purpleColour}[11]${endColour}${greenColour} find \-name *conf*${endColour} --> Busca en el directorio en el que se esta todos los archivos de configuracion "
+echo -e "${purpleColour}[12]${endColour}${greenColour} find \-name *back*${endColour} --> Busca en el directorio en el que se esta todos los archivos de backup "
+echo -e "${purpleColour}[13]${endColour}${greenColour} find \-user${endColour} ${blueColour}[usuario]${endColour} ${greenColour}2>/dev/null${endColour} --> Ver recursos de los que el usuario es propietario y elimina los errores "
+echo -e "${purpleColour}[14]${endColour}${greenColour} find \-perm -u=s 2>/dev/null${endColour} --> Busca archivos SUID que se ejecutan con permisos de propietario ${blueColour}[Normalmente root]${endColour}" 
+echo -e "${purpleColour}[15]${endColour} Puertos internos abiertos ${greenColour}proc/net/tcp${endColour} "
+echo -e "${purpleColour}[16]${endColour} Grupos en los que estamos ${greenColour}[ID]${endColour} --> Nos da el ID del grupo en el que esta el usuario que estamos usando"
+echo -e "${purpleColour}[17]${endColour} Tipo de archivo ${greenColour}file${endColour} ${blueColour}[archivo]${endColour} --> Nos dice de que tipo es un archivo o fichero "
+echo -e "${purpleColour}[18]${endColour}${greenColour} w${endColour} --> Para ver que usuarios estan conectados ahora mismo al sistema  \n"
 
 #if [ $1 -le "13" ]; then  #-le para menor o igual
 # opcion=$1 # seleccionamos por variable, no podemos dejar espacio
@@ -72,9 +123,9 @@ case "$opcion" in
 	7) if [ $# -ne 1 ]; then
 		 echo -e "Introduce comando \n"
 		 read -r comando
-		 watch -n 1 ls -l $comando
+		 watch -n 1 $comando
 	   else
-		 watch -n 1 ls -l $1 # 1 es el primer parametro que le pasamos
+		 watch -n 1 $1 # 1 es el primer parametro que le pasamos
 	   fi
 	;;
 	8) if [ $# -ne 1 ]; then
@@ -95,13 +146,85 @@ case "$opcion" in
 	   fi
 	 	
 	;;
-	10) if [ $# -ne 1 ]; then
-		 find \-name *config*
-	   else
-		 find $1 -name *config* # la ruta que le pasamos
-	   fi
+	10) if [ $# -ne 1 ]; then 
+
+		 	old_tree=$(tree ./ -a)
+		 	tree -a
+		 	while true; do
+		 	new_tree=$(tree ./ -a)
+		 	diff <(echo "$old_tree") <(echo "$new_tree") | grep [\<\>]
+		 	old_tree=$new_tree
+		 	done
+	    else
+	     	old_tree=$(tree $1 -a)
+		 	while true; do
+			new_tree=$(tree $1 -a)
+			diff <(echo "$old_tree") <(echo "$new_tree") | grep [\<\>]
+			old_tree=$new_tree
+			done	
+		fi
 	;;
 	11) if [ $# -ne 1 ]; then
+		 find \-name "*conf*"
+		 echo -e "\n ¿Quieres buscar usuarios y contraseñas en los archivos de configuracion? [S/N] \n"
+		 read fichero
+		 	if [ $fichero == "S" ]; then
+		 		echo
+		 		find ./ -type f -name "*conf*" -exec grep -riE "user|usuario|pass|contraseña|root|admin|creden" {} + 
+		 	fi
+		 echo -e "\n ¿Quieres buscar usuarios y contraseñas en todos los archivos? [S/N] \n"
+		 read fichero
+		 		 	if [ $fichero == "S" ]; then
+						echo
+		 		 		find ./ -name "*conf*" | grep -R -i -E "user|usuario|pass|contraseña|root|admin|creden" ./ 
+		 		 	fi
+	    else
+		    find $1 -name "*conf*" # la ruta que le pasamos
+		    echo -e "\n ¿Quieres buscar usuarios y contraseñas en los archivos de configuracion? [S/N] \n"
+		    read fichero
+		       if [ $fichero == "S" ]; then
+				    echo
+		 		    find $1 -type f -name "*conf*" -exec grep -riE "user|usuario|pass|contraseña|root|admin|creden" {} + 
+		 	   fi
+		    echo -e "\n ¿Quieres buscar usuarios y contraseñas en todos los archivos? [S/N] \n"
+		    read fichero
+		 	   if [ $fichero == "S" ]; then
+		 	 		echo
+		      	    find $1 -name "*conf*" | grep -R -i -E "user|usuario|pass|contraseña|root|admin|creden" $1
+		 	   fi
+	   fi
+	;;
+	12) if [ $# -ne 1 ]; then
+			 find \-name "*back*"
+			 echo -e "\n ¿Quieres buscar usuarios y contraseñas en los archivos de backup? [S/N] \n"
+			 read fichero
+			 	if [ $fichero == "S" ]; then
+					echo
+			 		find ./ -type f -name "*back*" -exec grep -riE "user|usuario|pass|contraseña|root|admin|creden" {} + 
+			 	fi
+			 echo -e "\n ¿Quieres buscar usuarios y contraseñas en todos los archivos? [S/N] \n"
+			 read fichero
+			    if [ $fichero == "S" ]; then
+					echo
+			 		find ./ -name "*back*" | grep -R -i -E "user|usuario|pass|contraseña|root|admin|creden" ./ 
+			 	fi
+		else
+			 find $1 -name "*back*" # la ruta que le pasamos
+			 echo -e "\n ¿Quieres buscar usuarios y contraseñas en los archivos de backup? [S/N] \n"
+			 read fichero
+			 	if [ $fichero == "S" ]; then
+					echo
+			 		find $1 -type f -name "*back*" -exec grep -riE "user|usuario|pass|contraseña|root|admin|creden" {} + 
+			 	fi
+			 echo -e "\n ¿Quieres buscar usuarios y contraseñas en todos los archivos? [S/N] \n"
+			 read fichero
+			 	if [ $fichero == "S" ]; then
+			 		echo
+			      	find $1 -name "*back*" | grep -R -i -E "user|usuario|pass|contraseña|root|admin|creden" $1
+			 	fi
+       fi
+	;;
+	13) if [ $# -ne 1 ]; then
 		 echo -e "Introduce usuario \n"
 		 read -r usuario
 		 find \-user $usuario 2>/dev/null
@@ -119,17 +242,43 @@ case "$opcion" in
 		    fi
         fi
 	 ;;
-	 12) if [ $# -ne 1 ]; then
-	 	  find \-perm -u=s 2>/dev/null
+	 14) if [ $# -ne 1 ]; then
+	 	  echo -e "\n Buscando por: find \-perm -u=s | xargs ls -la  ([*] Funciona si no hay espacios) \n"
+	 	  find \-perm -u=s 2>/dev/null | xargs ls -la
+	 	  echo -e "\n Buscando por: find ./ -perm -u=s -exec ls -la {} +   \n"
+	 	  find ./ -perm -u=s -exec ls -la {} + 
+	 	  echo -e "\n ¿Quieres buscar en la raiz? [S/N] \n"
+		  read raiz
+		 	 if [ $raiz == "S" ]; then
+		 	    echo -e "\n Buscando por: find / -perm -u=s | xargs ls -la  \n"
+				find / -perm -u=s 2>/dev/null | xargs ls -la 
+				echo -e "\n Buscando por: find / -perm -u=s -exec ls -la {} +   \n"
+			    find / -perm -u=s -exec ls -la {} + 
+		 	 fi
 	 	 else
-	 	  find $1 -perm -u=s 2>/dev/null # para hacerlo desde la Raiz poner /
+	 	  echo -e "\n Buscando por: find" $1 "-perm -u=s | xargs ls -la ([*] Funciona si no hay espacios) \n"
+	 	  find $1 -perm -u=s 2>/dev/null | xargs ls -la  # para hacerlo desde la Raiz poner /
+	 	  echo -e "\n Buscando por: find" $1 "-perm -u=s -exec ls -la {} +   \n"
+	 	  find $1 -perm -u=s -exec ls -la {} + 
+          echo -e "\n ¿Quieres buscar en la raiz? [S/N] \n"
+		  read raiz
+		 	 if [ $raiz == "S" ]; then
+				echo -e "\n Buscando por: find / -perm -u=s | xargs ls -la  \n"
+				find / -perm -u=s 2>/dev/null | xargs ls -la 
+				echo -e "\n Buscando por: find / -perm -u=s -exec ls -la {} +   \n"
+				find / -perm -u=s -exec ls -la {} + 
+		 	 fi
 	 	 fi
 	 ;;
-	 13) cat /proc/net/tcp | awk '{print $2}' | grep -v local | tr ': ' ' ' | awk 'NF{print $NF}'
+	 15) cat /proc/net/tcp | awk '{print $2}' | grep -v local | tr ': ' ' ' | awk 'NF{print $NF}'
+	 	 echo
+	 	 for hexadecimal in $(cat /proc/net/tcp | awk '{print $2}' | grep -v local | tr ': ' ' ' | awk 'NF{print $NF}'); do
+	 	 printf "El puerto $hexadecimal es el puerto: %d \n" $((16#$hexadecimal))
+	 	 done;
 	 ;;
-	 14) id
+	 16) id
 	 ;;
-	 15) if [ $# -ne 1 ]; then
+	 17) if [ $# -ne 1 ]; then
 	 	 	 echo -e "Introduce archivo \n"
 	 	 	 read -r archivo
 	 	 	 file $archivo
@@ -137,7 +286,7 @@ case "$opcion" in
 	 	 	 file $1 # 1 es el primer parametro que le pasamos
 	 	 fi
 	 ;;
-	 16) watch -n 1 'w'
+	 18) watch -n 1 'w'
 	 ;;		  
 esac
 
